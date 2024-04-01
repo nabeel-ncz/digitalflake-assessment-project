9ab2320e059a68ed2d7a3ba05453ab423c8c90f3
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-04-01 21:30:31 +0530

    fix: login validation

--- server/src/utils/validation/login.ts
+++ server/src/utils/validation/login.ts
@@ -7,7 +7,5 @@ export const loginSchema = Joi.object({
         .required(),
 
     password: Joi
-        .string()
-        .pattern(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/))
-        .required(),
+        .string().required(),
 })
